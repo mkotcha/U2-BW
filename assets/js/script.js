7900e93718838c6ab44cda8125c29ad331752661
Matteo Falasca <matteo.falasca@emmek.org>
2023-08-29 17:48:08 +0200

    workink on sidebar

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ const options = {
   },
 };
 let queryStr = "";
+const maxCard = 9;
 
 async function query(query) {
   try {
@@ -21,30 +22,36 @@ async function query(query) {
 window.onload = async () => {
   const recent = await query("playlist/752286631");
   const elm = document.querySelector(".recent-list");
+  const side = await query("playlist/10361569942");
+  printCard(elm, recent);
+  printSideCard(side);
+  hideCard();
+};
 
-  for (let i = 0; i < 9; i++) {
+const printCard = (elm, data) => {
+  for (let i = 0; i < maxCard; i++) {
     const col = document.createElement("div");
     col.classList = "col";
     const card = document.createElement("div");
     card.classList = "home-card rounded-2 d-flex flex-column p-3 h-100";
     const albumLink = document.createElement("a");
-    albumLink.href = "album.html?id=" + recent.tracks.data[i].album.id;
+    albumLink.href = "album.html?id=" + data.tracks.data[i].album.id;
     const img = document.createElement("img");
     img.classList = "rounded-2 mb-3 w-100";
-    img.src = recent.tracks.data[i].album.cover_medium;
-    img.alt = recent.tracks.data[i].album.title + " cover";
+    img.src = data.tracks.data[i].album.cover_medium;
+    img.alt = data.tracks.data[i].album.title + " cover";
     const title = document.createElement("p");
     title.classList = "fs-6 fw-bold m-0 mb-1 text-truncate";
     const titleLink = document.createElement("a");
     titleLink.classList = "text-reset text-decoration-none";
-    titleLink.href = "track.html/id=" + recent.tracks.data[i].id;
-    titleLink.innerText = recent.tracks.data[i].title;
+    titleLink.href = "track.html/id=" + data.tracks.data[i].id;
+    titleLink.innerText = data.tracks.data[i].title;
     const artist = document.createElement("p");
     artist.classList = "fs-6 fw-bold m-0 mb-1 text-truncate";
     const artistLink = document.createElement("a");
     artistLink.classList = "text-reset text-decoration-none";
-    artistLink.href = "track.html?id=" + recent.tracks.data[i].id;
-    artistLink.innerText = recent.tracks.data[i].artist.name;
+    artistLink.href = "track.html?id=" + data.tracks.data[i].id;
+    artistLink.innerText = data.tracks.data[i].artist.name;
 
     albumLink.appendChild(img);
     card.appendChild(albumLink);
@@ -55,7 +62,15 @@ window.onload = async () => {
     col.appendChild(card);
     elm.appendChild(col);
   }
-  hideCard();
+};
+
+const printSideCard = data => {
+  //   fetch("https://deezerdevs-deezer.p.rapidapi.com/playlist/10361569942", options)
+  //     .then(resp => resp.json())
+  //     .then(data => console.log(data))
+  //     .catch(error => console.log(error));
+  const list = document.querySelector(".side-list");
+  list.innerHtml = "";
 };
 
 const numCol = () => {
@@ -66,8 +81,8 @@ const numCol = () => {
 const hideCard = () => {
   const num = parseInt(numCol());
   const cards = document.querySelectorAll(".recent-list .col");
-  if (num < 10) cards[0].parentElement.classList = `row row-cols-${num} g-4 recent-list`;
-  else cards[0].parentElement.classList = `row row-cols-9 g-4 recent-list`;
+  if (num < maxCard) cards[0].parentElement.classList = `row row-cols-${num} g-0 recent-list`;
+  else cards[0].parentElement.classList = `row row-cols-${maxCard} g-0 recent-list`;
   cards.forEach((elm, index) => {
     if (index >= num) {
       elm.classList.add("d-none");
