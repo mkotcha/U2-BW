3cd1e6f3734c7f51bd6124e6ff0aeec7db85dda7
Matteo Falasca <matteo.falasca@emmek.org>
2023-08-31 14:50:31 +0200

    working on search

--- assets/js/search.js
+++ assets/js/search.js
@@ -26,20 +26,22 @@ async function deezerQuery(query) {
 
 async function printDiscover() {
   const discoverData = await deezerQuery("editorial/0/releases");
-  console.log(discoverData);
   const container = document.querySelector(".search-view-container");
   container.innerHTML = "";
   discoverData.data.forEach((data, i) => {
     container.innerHTML += `<div class="search-card .col">
-    <div class="ratio ratio-1x1 bg-danger rounded-3 overflow-hidden">
-      <div>
-        <p>${data.title}</p>
-        <img src="${data.cover_medium}" class="" alt="" />
-      </div>
+    <div class="search-card-int ratio ratio-1x1 bg-danger rounded-3 overflow-hidden">
+      <a href="" class="text-reset text-decoration-none">
+        <p class="fw-bold p-3 fs-4">${data.title}</p>
+        <img src="${data.cover_medium}" class="" alt="" crossorigin/>
+      </a>
     </div>
   </div>`;
   });
   console.log(document.querySelector(".search-card img"));
   const rgb = getAverageRGB(document.querySelector(".search-card img"));
-  console.log(rgb);
+  console.log(rgb.r);
+  const div = container.querySelector("div:last-child");
+  div.style = "background-color: blue";
+  console.log(div);
 }
